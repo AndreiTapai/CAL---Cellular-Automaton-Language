6ae18ad05164313d728a131c33d56d7ade782c7d
Andrei <Andrei@160.39.10.22>
2015-05-08 00:57:01 -0400

    Changed the Tester and GUI a little bit

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -1,3 +1,9 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
 /**
  * Test main goes in here.
  * 
@@ -7,7 +13,33 @@
 
 public class TestDriver {
 	public static void main(String[] args) {
-		LifeCAL lc = new LifeCAL();
-		GUI gui = new GUI(lc);
+            Class cl = null;
+            Object instance = null;
+                if (args.length != 1)
+                {
+                    System.out.println("You must include the class name as an argument");
+                    System.exit(-1);
+                }
+                else
+                {
+                    String className = args[0];
+                    try {
+                        cl = Class.forName(className);
+                    } catch (ClassNotFoundException ex) {
+                        ex.printStackTrace();
+                    }
+                    
+                try {
+                    instance = cl.newInstance();
+                } catch (InstantiationException ex) {
+                    ex.printStackTrace();
+                } catch (IllegalAccessException ex) {
+                    ex.printStackTrace();
+                }
+
+                    GUI gui = new GUI(instance);
+                }
+            
+		
 	}
 }
