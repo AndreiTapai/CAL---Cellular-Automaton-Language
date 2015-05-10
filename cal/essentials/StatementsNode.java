58c860034dce41b096cd7b2af21385fa073ce138
gml2151 <gml2151@columbia.edu>
2015-05-10 05:52:38 +0000

    updated

--- cal/essentials/StatementsNode.java
+++ cal/essentials/StatementsNode.java
@@ -2,10 +2,24 @@ package cal.essentials;
 
 public class StatementsNode extends AbstractNode{
 
+	private String sreturn;
+	private StatementsNode stmts;
+	private StatementNode stmt;
+	
+	public StatementsNode (StatementNode s){
+		stmt = s;
+		sreturn = stmt.toJava();
+	}
+	
+	public StatementsNode (StatementNode s, Statements Node s1){
+		stmt = s;
+		stmts = s1;
+		sreturn = stmt.toJava() + stmts.toJava();
+	}
 
 	@Override 
 	public String toJava(){
-		return ";\n" ;
+		return sreturn;
 	}
 
 
