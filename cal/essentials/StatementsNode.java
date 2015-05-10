496bbf7c5869e1e56a73c7d9f9352c5a39ca45e0
Percee Goings <pbg2111@columbia.edu>
2015-05-10 02:41:03 -0400

    StatementsNode class toJava

--- cal/essentials/StatementsNode.java
+++ cal/essentials/StatementsNode.java
@@ -2,13 +2,20 @@ package cal.essentials;
 
 public class StatementsNode extends AbstractNode{
 
-	
-	public StatementsNode(StatementNode... nodes){
+	ArrayList<StatementNode> statements;
 
+	public StatementsNode(StatementNode node, ArrayList<StatementNode> statements){
+		this.statements = new ArrayList<StatementNode>();
+		this.statements.add(node);
+		for(StatementsNode statement: statements)
+			this.statements.add(statement);
 	}
 
 	@Override 
 	public String toJava(){
+		String sval = "";
+		for(StatementsNode statement: statements)
+			sval += statement.toJava();
 		return ";\n" ;
 	}
 
