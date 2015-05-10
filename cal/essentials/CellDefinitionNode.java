748bd6c06f7b9f70b44b970095a05c29b56c9c4e
gml2151 <gml2151@columbia.edu>
2015-05-10 17:58:43 +0000

    toJava() edited; still need property type

--- cal/essentials/CellDefinitionNode.java
+++ cal/essentials/CellDefinitionNode.java
@@ -2,15 +2,22 @@ package cal.essentials;
 
 public class CellDefinitionNode extends AbstractNode {
 
+	private String sreturn;
 	private String type;
 
 	public CellDefinitionNode(String type) {
 		this.type = type;
+		if (type == "LIFE"){
+			sreturn = "public boolean life;\n";
+		}
+		else{
+			sreturn = "public " + /*type here*/ + type + ";\n";
+		}
 	}
 
+	/*print to FileNameCell.java*/
 	@Override
 	public String toJava() {
-		
+		return sreturn;
 	}
-
 }
