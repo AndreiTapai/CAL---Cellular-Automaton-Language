97e01354583e725e26d096a40125aca78d30b66a
Andrei <Andrei@160.39.10.22>
2015-05-10 12:46:36 -0400

    updated GridDefinitionJava

--- cal/essentials/GridDefinitionNode.java
+++ cal/essentials/GridDefinitionNode.java
@@ -5,9 +5,11 @@ public class GridDefinitionNode extends AbstractNode {
 	private String name;
 	private String type;
 	private int[] gridSize;
+        boolean isGSize;
 
 	public GridDefinitionNode(String name, String type, boolean isGridsize) {
 		gridSize = new int[2];
+                isGSize = isGridsize;
 		if (isGridsize) {
 			gridSize[0] = Integer.parseInt(type.split("x")[0]);
 			gridSize[1] = Integer.parseInt(type.split("x")[1]);
@@ -23,6 +25,19 @@ public class GridDefinitionNode extends AbstractNode {
 	public String toJava() {
 		// output some java code that translates to either grid g is GRIDSIZE
 		// or g is square (ignore other gridtypes)
+            String javaCode = "";
+            
+            if (isGSize)
+            {
+                javaCode = "public int gridgx = " + gridSize[0] + ";" + "\n" +
+                            "public int gridgy = " + gridSize[1] + ";" + "\n";
+            }
+            else
+            {
+                javaCode = "\n";
+            }
+            
+            return javaCode;
 	}
 
 }
