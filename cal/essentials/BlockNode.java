a110a8126632fa7e7d7eafe3ae44c99416f257d2
Fei-Tzin Lee <feitzin@gmail.com>
2015-05-10 18:26:51 -0400

    javadocs in BlockNode, CellDefinitionNode

--- cal/essentials/BlockNode.java
+++ cal/essentials/BlockNode.java
@@ -1,25 +1,44 @@
 package cal.essentials;
 
+/**
+ * This class represents a block of code.
+ */
 public class BlockNode extends AbstractNode {
-
 	private String sreturn;
 	private StatementsNode stmts;
 	private StatementNode stmt;
-
+	
+	/**
+	 * Constructor. Takes the Node for a series of statements.
+	 * 
+	 * @param s The StatementsNode representing the statements within this
+	 * block.
+	 */
 	public BlockNode(StatementsNode s) {
 		stmts = s;
 		sreturn = "{\n" + stmts.toJava() + "}\n";
 	}
-
+	
+	/**
+	 * Constructor. Takes the Node for a single statement.
+	 * 
+	 * @param s The StatementNode representing the statement in this block.
+	 */
 	public BlockNode(StatementNode s) {
 		stmt = s;
 		sreturn = "{\n" + stmt.toJava() + "}\n";
 	}
 
+	/**
+	 * Constructor for an empty block.
+	 */
 	public BlockNode() {
 		sreturn = "{\n}\n";
 	}
 
+	/**
+	 * toJava() method. Returns the code for this block.
+	 */
 	@Override
 	public String toJava() {
 		return sreturn;
--- cal/essentials/CellDefinitionNode.java
+++ cal/essentials/CellDefinitionNode.java
@@ -1,11 +1,21 @@
 package cal.essentials;
 
+/**
+ * This class represents a definition of a Cell variable.
+ */
 public class CellDefinitionNode extends AbstractNode {
-
 	private String sreturn;
 	private String type;
 	private String name;
-
+	
+	/**
+	 * Constructor. Takes a variable type and name and returns the appropriate
+	 * Java code (which will go within the definition of the Cell Java class).
+	 * 
+	 * @param type The type of the variable (may be the reserved keyword LIFE,
+	 * or any CAL type.)
+	 * @param name The name of the variable.
+	 */
 	public CellDefinitionNode(String type, String name) {
 		this.type = type;
 		this.name = name;
@@ -16,6 +26,9 @@ public class CellDefinitionNode extends AbstractNode {
 		}
 	}
 
+	/**
+	 * toJava() method.
+	 */
 	/* print to FileNameCell.java */
 	@Override
 	public String toJava() {
