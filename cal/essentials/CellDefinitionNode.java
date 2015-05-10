48b64198f23b2827c31fc41176843adf16e34dad
gml2151 <gml2151@columbia.edu>
2015-05-10 18:08:04 +0000

    Updated toJava(); should work now

--- cal/essentials/CellDefinitionNode.java
+++ cal/essentials/CellDefinitionNode.java
@@ -4,14 +4,16 @@ public class CellDefinitionNode extends AbstractNode {
 
 	private String sreturn;
 	private String type;
+	private String name;
 
-	public CellDefinitionNode(String type) {
+	public CellDefinitionNode(String type, String name) {
 		this.type = type;
+		this.name = name;
 		if (type == "LIFE"){
 			sreturn = "public boolean life;\n";
 		}
 		else{
-			sreturn = "public " + /*type here*/ + type + ";\n";
+			sreturn = "public " + type + " " + name + ";\n";
 		}
 	}
 
