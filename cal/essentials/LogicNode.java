8d85ff1a64555a63c719e26daf8ce42dd8c84498
gml2151 <gml2151@columbia.edu>
2015-05-12 04:02:25 +0000

    added documentation to toString()/toJava()

--- cal/essentials/LogicNode.java
+++ cal/essentials/LogicNode.java
@@ -22,11 +22,21 @@ public class LogicNode extends AbstractNode {
 		 */
 	}
 
+	/*
+	 * toString() returns the LogicNode's sval, which was passed in the constructor.
+	 * It does NOT return the Java code--this is so we can handle nand/nor and or/xor/and
+	 * differently. toString just returns "and", "or", etc.
+	 */
 	@Override
 	public String toString() {
 		return logicOp;
 	}
 
+	/*
+	 * toJava() returns the Java code--"&&", "||", etc. This does not handle nand/nor
+	 * which have different formats. These are handled by calling toString() in 
+	 * the ConditionalNode constructor.
+	 */
 	@Override
 	public String toJava() {
 		return sreturn;
